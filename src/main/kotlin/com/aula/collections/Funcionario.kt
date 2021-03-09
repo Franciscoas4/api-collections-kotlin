260daa025830ec8d7361254e97c41aebdc7dae8a
Franciscoas4 <franciscoas4@gmail.com>
2021-03-09 20:46:09 -0300

    update test list, extract employee to separated file and created test set,

---
+++ src/main/kotlin/com/aula/collections/Funcionario.kt
@@ -0,0 +1,15 @@
+package com.aula.collections
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salário: $salario
+        """.trimIndent()
+
+}
---
+++ src/main/kotlin/com/aula/collections/TesteList.kt
@@ -0,0 +1,25 @@
+package com.aula.collections
+
+fun main() {
+    val joao = Funcionario("João", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val funcionarios = listOf(joao, pedro, maria)
+
+    funcionarios.forEach { println(it)}
+
+    println("---------------Utilizando find--------------------")
+    println(funcionarios.find { it.nome == "Maria" })
+
+    println("----------------Utilizando sortedBy-------------------")
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it)}
+
+    println("----------------Utilizando groupBy-------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach { println(it)}
+
+}
